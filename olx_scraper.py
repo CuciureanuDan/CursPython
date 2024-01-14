0bd3ed0351a3f4929af2e3c3b62017feedcac22f
CuciureanuDan <dancuciureanu4@yahoo.com>
2024-01-14 23:39:58 +0200

    new sectiom

--- olx_scraper.py
+++ olx_scraper.py
@@ -1,10 +1,27 @@
-# """
-# Extragem din fisierul cu extensia .ini link-ul site-ului olx , numele produsului cautat 
-# """
+"""
+  Extragem din fisierul cu extensia .ini link-ul site-ului olx , numele produsului cautat 
+"""
 
 from configparser import ConfigParser
 import requests
 from bs4 import BeautifulSoup
+
+
+def extragereNr(x):
+    """
+    Dintr-un sir de caractere extrage doar numerele
+    
+    Args:
+        x (str) : un sir de caractere
+
+    Return:
+        int: Numerele extrase din sir
+        -1: Daca nu exista numere
+
+    """
+    sir=''.join(c for c in x if c.isdigit())
+    return int(sir) if sir else -1
+
 
 config = ConfigParser()
 
@@ -24,15 +41,32 @@ response = requests.get(url)
 #</div>
 
 
-
+produse=[]
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text,'html.parser')
 
-
-
+    divs = soup.find_all('div', class_='css-u2ayx9')
+    #aceste divuri trebuie sortate in functie de pret , dar pretul trebuie extras numai cu cifre
+    # o sa facem o lista cu dictionare care sa contina titlul si pretul , iar dupa o sa fie usor si de sortat 
+    
+    for div in divs:
+        elem={}
+        title = div.find('h6')
+        price = div.find('p')
+        elem['titlu'] = title.get_text()
+        elem['pret'] = extragereNr(price.get_text())
+        produse.append(elem)
 else:
     print("Cererea nu a fost reusita. Cod de stare:", response.status_code)
+
+    #facem sortarea si afisarea produselor in consola
+if produse: 
+    produse.sort(key=lambda x: x['pret'])
+    for aux in produse:
+        # print("Titlu: "+aux['titlu']+"\n"
+        #       +"Pret: "+str(aux['pret'])+" lei" )
+        print(f"Titlu:  {aux['titlu']} \n Pret: {aux['pret']} lei \n")
 
 
 
